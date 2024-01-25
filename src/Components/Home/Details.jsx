5a7f870de6ad0a68bd00917e5964a65620e08b1c
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2024-01-25 22:59:34 +0600

    Project Updated

--- src/Components/Home/Details.jsx
+++ src/Components/Home/Details.jsx
@@ -8,8 +8,8 @@ const Details = () => {
                 <div className="hero-overlay bg-opacity-60"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                        <p className="mb-5 text-xl">Explore cutting-edge innovations, network with tech enthusiasts, and witness groundbreaking projects at our Tech Fair Event. Unleash the future!</p>
+                        <h1 className="mb-5 text-5xl font-bold">Hello There</h1>
+                        <p className="mb-5 text-xl">Explore Cutting-edge innovations, network with tech enthusiasts, and witness groundbreaking projects at our Tech Fair Event. Unleash the future!</p>
                        <Link to='/'><button className="btn btn-primary text-xl">Go Back</button></Link>
                     </div>
                 </div>
--- src/Components/Home/Event.jsx
+++ src/Components/Home/Event.jsx
@@ -6,7 +6,7 @@ const Event = () => {
     return (
         <div className="card hover:bg-blue-100 hover:cursor-pointer  text-neutral border border-blue-300" data-aos="fade-up" data-aos-duration="1500">
             <div className="card-body items-center text-center">
-                <h2 className="card-title text-2xl">Upcoming Events!</h2>
+                <h2 className="card-title text-2xl">Upcoming Events</h2>
                 <p>Exciting & Tech Related Events Happenig</p>
                 <div className="card-actions justify-end">
                    <Link to='/details'><button className="btn bg-blue-300 text-white text-xl">View Details</button></Link>
