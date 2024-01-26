f7750991b7d6d2f7b19a1f380a0b574aabe2b047
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2024-01-26 22:43:37 +0600

    Member Section Edited

--- src/Components/Home/Member.jsx
+++ src/Components/Home/Member.jsx
@@ -10,6 +10,8 @@ const Member = () => {
                 <Event></Event>
             </div>
 
+            {/* All Members */}
+            
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12'>
                 <img className='rounded-xl' src={member} alt="" data-aos="zoom-in" data-aos-duration="1500"/>
                 <img className='rounded-xl' src={member} alt="" data-aos="zoom-in" data-aos-duration="1500"/>
