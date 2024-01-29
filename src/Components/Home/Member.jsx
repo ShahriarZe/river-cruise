4db8806388638533ca019107e304f77cfb54bcdc
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2024-01-29 22:15:49 +0600

    Member Section Edited

--- src/Components/Home/Member.jsx
+++ src/Components/Home/Member.jsx
@@ -16,9 +16,9 @@ const Member = () => {
                 <img className='rounded-xl' src={member} alt="member1" data-aos="zoom-in" data-aos-duration="1500"/>
                 <img className='rounded-xl' src={member} alt="member2" data-aos="zoom-in" data-aos-duration="1500"/>
                 <img className='rounded-xl' src={member} alt="member3" data-aos="zoom-in" data-aos-duration="1500"/>
-                <img className='rounded-xl' src={member} alt="member4" data-aos="zoom-out" data-aos-duration="1500"/>
-                <img className='rounded-xl' src={member} alt="member5" data-aos="zoom-out" data-aos-duration="1500"/>
-                <img className='rounded-xl' src={member} alt="member6" data-aos="zoom-out" data-aos-duration="1500"/>
+                <img className='rounded-xl' src={member} alt="Member4" data-aos="zoom-out" data-aos-duration="1500"/>
+                <img className='rounded-xl' src={member} alt="Member5" data-aos="zoom-out" data-aos-duration="1500"/>
+                <img className='rounded-xl' src={member} alt="Member6" data-aos="zoom-out" data-aos-duration="1500"/>
             </div>
         </div>
     );
