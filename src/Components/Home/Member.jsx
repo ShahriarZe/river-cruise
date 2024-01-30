3998cfbeacce0d10030ab787e622ce87b62abe84
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2024-01-30 23:47:57 +0600

    Member Section Edited

--- src/Components/Home/Member.jsx
+++ src/Components/Home/Member.jsx
@@ -13,9 +13,9 @@ const Member = () => {
             {/* All Members */}
             
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12'>
-                <img className='rounded-xl' src={member} alt="member1" data-aos="zoom-in" data-aos-duration="1500"/>
-                <img className='rounded-xl' src={member} alt="member2" data-aos="zoom-in" data-aos-duration="1500"/>
-                <img className='rounded-xl' src={member} alt="member3" data-aos="zoom-in" data-aos-duration="1500"/>
+                <img className='rounded-xl' src={member} alt="Member1" data-aos="zoom-in" data-aos-duration="1500"/>
+                <img className='rounded-xl' src={member} alt="Member2" data-aos="zoom-in" data-aos-duration="1500"/>
+                <img className='rounded-xl' src={member} alt="Member3" data-aos="zoom-in" data-aos-duration="1500"/>
                 <img className='rounded-xl' src={member} alt="Member4" data-aos="zoom-out" data-aos-duration="1500"/>
                 <img className='rounded-xl' src={member} alt="Member5" data-aos="zoom-out" data-aos-duration="1500"/>
                 <img className='rounded-xl' src={member} alt="Member6" data-aos="zoom-out" data-aos-duration="1500"/>
